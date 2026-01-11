eb79a0afbf96d7851f0f90b421dcf2c10bf87534
harshika <harshikakolekar84@gmail.com>
2026-01-11 20:09:31 +0530

    Setting up portfolio

---
+++ script.js
@@ -0,0 +1,36 @@
+const toggleBtn = document.querySelector(".theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+  // Dark mode will be added later
+  alert("Dark mode coming next 👀");
+});
+
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+reveals.forEach(reveal => {
+  observer.observe(reveal);
+});
+
+const scrollTopBtn = document.querySelector(".scroll-top");
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
